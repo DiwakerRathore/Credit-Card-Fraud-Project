c5ba6106394838fb5977c6b3352765dd4809ee5a
Diwaker Rathore <rathore710diwaker@gmail.com>
2023-04-15 23:52:03 +0530

    Model

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,6 @@
 import sys
 import logging
+from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -25,3 +26,4 @@ if __name__=="__main__":
     except Exception as e:
         logging.info("divide by zero")
         raise CustomException(e,sys)
+    
